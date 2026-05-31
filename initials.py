4ae0ba0e6c3ef5fdaf454966d49046df88491ba5
Asomani Jeffery Kye <287986770+aj-404-ux@users.noreply.github.com>
2026-05-31 01:15:32 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Asomani Jeffery Kye
+# STUDENT ID: 2425402876
+# Email: 2425402876@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Asomani Jeffery Kye, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in stylized large letters
 
+    print()
+    print("...AJK")
+    print()
+
+    print("...AAAAAAAAAAAA...JJJJJJJJJJJJ...KKK......KKK...****")
+    print("...AAA......AAA...JJJJJJJJJJJJ...KKK....KKK.....****")
+    print("...AAA......AAA.........JJ.......KKK..KKK.......****")
+    print("...AAA......AAA.........JJ.......KKKKKK.........****")
+    print("...AAAAAAAAAAAA.........JJ.......KKKKKK.........****")
+    print("...AAA......AAA.........JJ.......KKKKKK.........****")
+    print("...AAA......AAA.........JJ.......KKK..KKK.......****")
+    print("...AAA......AAA.........JJ.......KKK....KKK.....****")
+    print("...AAA......AAA...JJJJJJJJ.......KKK......KKK...****")
+    print("...AAA......AAA...JJJJJJJJ.......KKK......KKK...****")
+
+    print()
 
 
 main()
